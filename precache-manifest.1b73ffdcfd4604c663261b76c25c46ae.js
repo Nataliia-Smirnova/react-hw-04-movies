19f0c5ebe985ac7329ff25f2f3bfadb623a5677d
Nataliia <n3v3st4@gmail.com>
2020-09-13 17:30:43 +0300

    Updates

---
+++ precache-manifest.1b73ffdcfd4604c663261b76c25c46ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c4899cc75be2afd76be0b8d3f4f6ef",
+    "url": "/react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "6694ff201b886288db7c",
+    "url": "/react-hw-04-movies/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "6694ff201b886288db7c",
+    "url": "/react-hw-04-movies/static/js/2.ccbdc61b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-04-movies/static/js/2.ccbdc61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c6e2090fd3124b1fb0",
+    "url": "/react-hw-04-movies/static/js/main.0d3f5aec.chunk.js"
+  },
+  {
+    "revision": "299fa0c3623e30db6171",
+    "url": "/react-hw-04-movies/static/js/runtime-main.53acfac7.js"
+  }
+]);
